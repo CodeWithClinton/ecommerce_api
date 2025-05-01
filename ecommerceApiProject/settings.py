6d94fe81f8e7382813ae2a6fb1226ee71d89d668
CodeWithClinton <clintonmatics@gmail.com>
2025-05-01 06:35:56 +0100

    allowed origins

--- ecommerceApiProject/settings.py
+++ ecommerceApiProject/settings.py
@@ -96,33 +96,33 @@ DB = os.environ.get("DB")
 
 
 
-DATABASES = {
-            'default': {
-                'ENGINE': 'django.db.backends.sqlite3',
-                'NAME': BASE_DIR / 'db.sqlite3',
-            }
-        }
-
-# if DB in ["False", False]:
-
-#     DATABASES = {
+# DATABASES = {
 #             'default': {
 #                 'ENGINE': 'django.db.backends.sqlite3',
 #                 'NAME': BASE_DIR / 'db.sqlite3',
 #             }
 #         }
+
+if DB in ["False", False]:
+
+    DATABASES = {
+            'default': {
+                'ENGINE': 'django.db.backends.sqlite3',
+                'NAME': BASE_DIR / 'db.sqlite3',
+            }
+        }
     
-# else:
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.postgresql',
-#             'NAME': os.environ.get("DB_NAME"),
-#             'USER': os.environ.get("DB_USER"),
-#             'PASSWORD': os.environ.get("DB_PASSWORD"),
-#             'HOST': os.environ.get("DB_HOST"),  
-#             'PORT': os.environ.get("DB_PORT"),     
-#         }
-#     }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.environ.get("DB_NAME"),
+            'USER': os.environ.get("DB_USER"),
+            'PASSWORD': os.environ.get("DB_PASSWORD"),
+            'HOST': os.environ.get("DB_HOST"),  
+            'PORT': os.environ.get("DB_PORT"),     
+        }
+    }
 
 
 
